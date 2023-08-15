0c12446912e6e4284279adafc3a32287441ca02c
jadeam0 <jdpaulse27@gmail.com>
2023-08-15 14:27:10 +0200

    working

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+    db: {
+      /* don't expose password or any sensitive info, done only for demo */
+      host: "bxxorzk0wzuk5t95zyop-mysql.services.clever-cloud.com",
+      user: "ulpvqnnvdpdlakep",
+      password: "Yn3890Y1Z3qNNPpzniel",
+      database: "bxxorzk0wzuk5t95zyop",
+      connectTimeout: 60000
+    },
+    listPerPage: 10,
+  };
+  module.exports = config;
